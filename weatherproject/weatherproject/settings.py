1997e09a1bc8e3a01d88a9fe4423691f187a7b75
kpacyk <katarzyna.pacyk@gmail.com>
2022-10-21 00:59:44 +0200

    remove crispy forms

--- weatherproject/weatherproject/settings.py
+++ weatherproject/weatherproject/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "crispy_forms",
     "weatherapp.apps.WeatherappConfig",
     "register.apps.RegisterConfig",
 
