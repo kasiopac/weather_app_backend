5efc1b72d068546f389856da3b9f6e0a35290ef2
kpacyk <katarzyna.pacyk@gmail.com>
2022-10-20 23:56:40 +0200

    add user auth params

--- weatherproject/weatherproject/settings.py
+++ weatherproject/weatherproject/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "weatherapp",
+    "crispy_forms",
+    "weatherapp.apps.WeatherappConfig",
+    "register.apps.RegisterConfig",
+
 ]
 
 MIDDLEWARE = [
@@ -120,5 +123,7 @@ STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
-
+CRISPY_TEMPLATE_PACK="bootstrap4"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+LOGIN_REDIRECT_URL = "/mycity"
+LOGOUT_REDIRECT_URL = "/"
